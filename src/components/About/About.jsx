42a70c7446074b102313caedd178df8f52266957
luckylin villamor <luckysvilla.mor@gmail.com>
2025-06-09 00:55:41 -0400

    add about and footer content and styles

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -11,8 +11,8 @@ function About () {
     <p className="author__title">About the author</p>
     <p className="author__description">
     This block describes the project author. Here you should indicate your name, what you do, and which development technologies you know.
-
-You can also talk about your experience with TripleTen, what you learned there, and how you can help potential customers.
+    </p>
+<p>You can also talk about your experience with TripleTen, what you learned there, and how you can help potential customers.
     </p>
 </div>
 
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
 import '../Footer/Footer.css'
+import { Link } from 'react-router-dom'
+import fb from '../../assets/fb.svg'
+import github from '../../assets/github.svg'
+
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <footer className="footer">
+        <div className="footer__container">
+<p className="footer__text">© 2024 Supersite, Powered by News API </p>
+<div className="footer__links">
+    <Link className='footer__links-hyperlink footer__links-home'>Home</Link>
+    <Link className='footer__links-hyperlink footer__links-company`'>Tripleten</Link>
+    <Link className='footer__links-social'> <img src={github} alt="github link" /></Link>
+    <Link className='footer__links-social'><img src={fb} alt="facebook link" /></Link>
+</div>
+        </div>
+    </footer>
   )
 }
 
