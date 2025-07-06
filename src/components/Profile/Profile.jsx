8b4535c50da9226e589c6287b562ba2241d17f0b
luckylin villamor <luckysvilla.mor@gmail.com>
2025-07-06 00:01:58 -0400

    fix profile component

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -3,6 +3,8 @@ import "../Profile/Profile.css";
 import Main from "../Main/Main";
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
+import ItemsArray from "../../utils/ItemsArray"; // make sure this is correctly defined
+import NewsCard from "../NewsCard/NewsCard"; // assuming you're rendering cards
 
 function Profile({
 	handleSaveCard,
@@ -24,17 +26,31 @@ function Profile({
 			<section className="profile__content">
 				<div className="profile__text-container">
 					<p className="profile__subtitle">Saved articles</p>
-					<h3 className="profile__title">Elise, you have 5 saved articles</h3>
+					<h3 className="profile__title">
+						Elise, you have {savedNewsCards.length} saved articles
+					</h3>
 					<p className="profile__heading">
-						By keywords: Nature, Yellowstone, and 2 other
+						By keywords: Nature, Yellowstone, and 2 others
 					</p>
 				</div>
+				<div className="profile__saved-cards">
+					<div className="profile__news-cards-container">
+						<ul className="profile__news-cards-list">
+							{isLoggedIn &&
+								savedNewsCards.map((newsCard) => (
+									<NewsCard
+										key={newsCard._id}
+										newsCard={newsCard}
+										handleLoginClick={handleLoginClick}
+										handleSaveCard={handleSaveCard}
+										isLoggedIn={isLoggedIn}
+										handleRemoveCard={handleRemoveCard}
+									/>
+								))}
+						</ul>
+					</div>
+				</div>
 			</section>
-			<Main
-				handleSaveCard={handleSaveCard}
-				savedNewsCards={savedNewsCards}
-				handleRemoveCard={handleRemoveCard}
-			/>
 		</>
 	);
 }
