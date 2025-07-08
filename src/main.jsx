17a2e9d7622bb779fc0f0a27330a2fd7b1332848
luckylin villamor <luckysvilla.mor@gmail.com>
2025-07-08 12:01:45 -0400

    deploy changes

--- src/main.jsx
+++ src/main.jsx
@@ -2,16 +2,16 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+// import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 import "./index.css";
 import App from "./components/App/App";
 
 createRoot(document.getElementById("root")).render(
-
-  <React.StrictMode>
-    <BrowserRouter>
-    <App />
-    </BrowserRouter>
-  </React.StrictMode>,
+	<React.StrictMode>
+		<HashRouter>
+			<App />
+		</HashRouter>
+	</React.StrictMode>
 );
--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,12 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3000, // Change the port number to 3000
-  },
+	base:
+		process.env.NODE_ENV === "production"
+			? "/project_newsexplorer_frontend/"
+			: "/",
+	plugins: [react()],
+	server: {
+		port: 3000, // Change the port number to 3000
+	},
 });
