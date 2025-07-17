b3925f4d45c5928542dbf5bc9d55d981d6a464c4
luckylin villamor <luckysvilla.mor@gmail.com>
2025-07-17 16:51:26 -0400

    Fix external link paths with a href instead of link

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,28 +1,57 @@
-import React from 'react'
-import '../Footer/Footer.css'
-import { Link } from 'react-router-dom'
-import fb from '../../assets/fb.svg'
-import github from '../../assets/github.svg'
-
+import React from "react";
+import "../Footer/Footer.css";
+import { Link } from "react-router-dom";
+import fb from "../../assets/fb.svg";
+import github from "../../assets/github.svg";
 
 function Footer() {
-  return (
-    <footer className="footer">
-        <div className="footer__container">
-<p className="footer__text">© 2024 Supersite, Powered by News API </p>
-<div className="footer__links">
-    <div className="footer__links-hyperlink-container">
-    <Link className='footer__links-hyperlink footer__links-home'>Home</Link>
-    <Link className='footer__links-hyperlink footer__links-company`'>Tripleten</Link>
-    </div>
-    <div className="footer__links-social-container">
-    <Link className='footer__links-social'> <img src={github} alt="github link" /></Link>
-    <Link className='footer__links-social'><img src={fb} alt="facebook link" /></Link>
-    </div>
-</div>
-        </div>
-    </footer>
-  )
+	return (
+		<footer className="footer">
+			<div className="footer__container">
+				<p className="footer__text">© 2024 Supersite, Powered by News API </p>
+				<div className="footer__links">
+					<div className="footer__links-hyperlink-container">
+						<Link
+							className="footer__links-hyperlink footer__links-home"
+							// href="https://tripleten.com/"
+							target="_blank"
+						>
+							Home
+						</Link>
+						<a
+							className="footer__links-hyperlink footer__links-company`"
+							href="https://tripleten.com/"
+							target="_blank"
+						>
+							Tripleten
+						</a>
+					</div>
+					<div className="footer__links-social-container">
+						<a
+							className="footer__links-social"
+							href="https://github.com/luckygith/project_newsexplorer_frontend/"
+							target="_blank"
+						>
+							<img
+								src={github}
+								alt="github link"
+							/>
+						</a>
+						<a
+							className="footer__links-social"
+							href="https://facebook.com/"
+							target="_blank"
+						>
+							<img
+								src={fb}
+								alt="facebook link"
+							/>
+						</a>
+					</div>
+				</div>
+			</div>
+		</footer>
+	);
 }
 
-export default Footer
+export default Footer;
