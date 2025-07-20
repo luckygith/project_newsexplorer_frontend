0fc34454d2030474279506f355543c47c51b6e8a
luckylin villamor <luckysvilla.mor@gmail.com>
2025-07-19 23:41:41 -0400

    fix button submit

--- src/components/RegistrationConfirmedModal/RegistrationConfirmedModal.jsx
+++ src/components/RegistrationConfirmedModal/RegistrationConfirmedModal.jsx
@@ -1,28 +1,35 @@
-import React from 'react'
-import ModalwithForm from '../ModalWithForm/ModalwithForm'
+import React from "react";
+import ModalwithForm from "../ModalWithForm/ModalwithForm";
+import "../RegistrationConfirmedModal/RegistrationConfirmedModal.css";
 
-const RegistrationConfirmedModal = ({handleCloseModal, handleLoginClick}) => {
-  
-    const handleOrLoginClick = () => {
-        handleLoginClick();
-      };
-   
-    return (
-    <ModalwithForm
-    title="Registration succesfully completed!"
-    handleCloseModal={handleCloseModal}
-    // isOpen={isOpen}
-    // onSubmit={handleSubmit}
-    >
-        <button
-          type="button"
-          className="modal__to-register-button"
-          onClick={handleOrLoginClick}
-        >
-          Sign in
-        </button>
-    </ModalwithForm>
-  )
-}
+const RegistrationConfirmedModal = ({ handleCloseModal, handleLoginClick }) => {
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		handleOrLoginClick();
+	};
 
-export default RegistrationConfirmedModal
+	const handleOrLoginClick = () => {
+		handleLoginClick();
+	};
+
+	return (
+		<ModalwithForm
+			title="Registration succesfully completed!"
+			handleCloseModal={handleCloseModal}
+			// isOpen={isOpen}
+			onSubmit={handleSubmit}
+		>
+			<div className="registration-modal__content">
+				<button
+					type="button"
+					className="modal__to-register-button"
+					onClick={handleOrLoginClick}
+				>
+					Sign in
+				</button>
+			</div>
+		</ModalwithForm>
+	);
+};
+
+export default RegistrationConfirmedModal;
